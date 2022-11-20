cc19fe7ac7eeb5cdeaf25799876aca8f1ffc923c
Victor Su <victorsu@Victors-MBP.localdomain>
2022-11-20 14:41:48 -0500

    update_logic

--- logic.py
+++ logic.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 conn = pymysql.connect(host='localhost',
                        user='root',
                        password='',
-                       db='blog',
+                       db='air_ticket_reservation_system',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
 
